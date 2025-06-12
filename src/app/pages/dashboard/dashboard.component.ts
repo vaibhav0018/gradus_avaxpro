5e699b8d671dc18f098b4a60569a8b7518311b8d
sahilwaykar7 <sahilwaykar7@gmail.com>
2025-06-12 13:06:52 +0530

    sahil changes

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { FlexLayoutModule } from '@ngbracket/ngx-layout';
@@ -8,6 +8,8 @@ import { TodoComponent } from './todo/todo.component';
 import { DragulaModule } from 'ng2-dragula';
 import { InfoCardsComponent } from './info-cards/info-cards.component';
 import { LoginComponent } from "../../feature/non-session/login/login.component";
+import { Settings } from '@services/settings.service';
+import { AppSettings } from '../../app.settings';
 
 @Component({
     selector: 'app-dashboard',
@@ -22,9 +24,14 @@ import { LoginComponent } from "../../feature/non-session/login/login.component"
     templateUrl: './dashboard.component.html',
     styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
-    public icons = [ "home", "person", "alarm", "work", "mail", "favorite", "vaibhav"];
-    public colors = [ "accent", "primary", "warn", "warn-light", "accent-light", "primary-light" ];
+export class DashboardComponent implements OnInit {
+    // public icons = [ "home", "person", "alarm", "work", "mail", "favorite", "vaibhav"];
+    // public colors = [ "accent", "primary", "warn", "warn-light", "accent-light", "primary-light" ];
+    public settings: Settings
+    constructor(public appSettings: AppSettings) {
+      this.settings = this.appSettings.settings
+    }
+  
+    ngOnInit() {}
+  }
 
-
-}
